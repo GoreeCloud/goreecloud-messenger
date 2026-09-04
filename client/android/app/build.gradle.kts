4e38a8ab40af998a66d452e7df76f43e7d91e3e6
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-04 13:33:06 -0500

    Add Messenger Android runtime test dependencies

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -38,4 +38,7 @@ android {
 
 dependencies {
     testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:runner:1.7.0")
+    androidTestImplementation("androidx.test:core-ktx:1.7.0")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
 }
